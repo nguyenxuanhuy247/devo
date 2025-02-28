ceb20cf15d9da2a2c0eca1deaf20922d4c675cb8
nguyenxuanhuy247 <122721500+nguyenxuanhuy247@users.noreply.github.com>
2025-02-28 05:42:46 +0000

    12:45 - 28/02 : Test commit từ máy tính

--- src/app/features/time-tracking/bug-improvement-list/fix-bug-do-improvement.model.ts
+++ src/app/features/time-tracking/bug-improvement-list/fix-bug-do-improvement.model.ts
@@ -1,5 +1,6 @@
 import { IColumnHeaderConfigs } from '../../../shared/interface/common.interface';
 import { CommonService } from '../../../services';
+import { getHeaderColumnConfigsFactory } from '../time-tracking.model';
 
 export const FIX_BUG_DO_IMPROVEMENT_COLUMN_FIELD =
   CommonService.generateEnumFromInterface<IFixBugDoImprovementRowData>();
@@ -43,60 +44,79 @@ export const fixBugDoImprovementNullableObj: IFixBugDoImprovementRowData = {
   createdDate: '',
 };
 
-export const fixBugDoImprovementHeaderColumnConfigs: IColumnHeaderConfigs[] = [
-  {
-    label: 'STT',
-    field: FIX_BUG_DO_IMPROVEMENT_COLUMN_FIELD.no,
-    minWidth: 70,
-  },
-  {
-    label: 'Module',
-    field: FIX_BUG_DO_IMPROVEMENT_COLUMN_FIELD.moduleId,
-    minWidth: 200,
-  },
-  {
-    label: 'Menu',
-    field: FIX_BUG_DO_IMPROVEMENT_COLUMN_FIELD.menuId,
-    minWidth: 200,
-  },
-  {
-    label: 'Màn hình',
-    field: FIX_BUG_DO_IMPROVEMENT_COLUMN_FIELD.screenId,
-    minWidth: 200,
-  },
-  {
-    label: 'Tính năng',
-    field: FIX_BUG_DO_IMPROVEMENT_COLUMN_FIELD.featureId,
-    minWidth: 200,
-  },
-  {
-    label: 'Phân loại',
-    field: FIX_BUG_DO_IMPROVEMENT_COLUMN_FIELD.tabName,
-    minWidth: 200,
-  },
-  {
-    label: 'Mã bug & improvement',
-    field: FIX_BUG_DO_IMPROVEMENT_COLUMN_FIELD.code,
-    minWidth: 120,
-  },
-  {
-    label: 'Tên bug & improvement',
-    field: FIX_BUG_DO_IMPROVEMENT_COLUMN_FIELD.name,
-    minWidth: 120,
-  },
-  {
-    label: 'Thời gian bắt đầu',
-    field: FIX_BUG_DO_IMPROVEMENT_COLUMN_FIELD.startTime,
-    minWidth: 200,
-  },
-  {
-    label: 'Thời gian hoàn thành',
-    field: FIX_BUG_DO_IMPROVEMENT_COLUMN_FIELD.endTime,
-    minWidth: 200,
-  },
-  {
-    label: 'Thời lượng',
-    field: FIX_BUG_DO_IMPROVEMENT_COLUMN_FIELD.duration,
-    minWidth: 120,
-  },
-];
+// export const fixBugDoImprovementHeaderColumnConfigs: IColumnHeaderConfigs[] = [
+//   {
+//     label: 'STT',
+//     field: FIX_BUG_DO_IMPROVEMENT_COLUMN_FIELD.no,
+//     minWidth: 70,
+//   },
+//   {
+//     label: 'Module',
+//     field: FIX_BUG_DO_IMPROVEMENT_COLUMN_FIELD.moduleId,
+//     minWidth: 200,
+//   },
+//   {
+//     label: 'Menu',
+//     field: FIX_BUG_DO_IMPROVEMENT_COLUMN_FIELD.menuId,
+//     minWidth: 200,
+//   },
+//   {
+//     label: 'Màn hình',
+//     field: FIX_BUG_DO_IMPROVEMENT_COLUMN_FIELD.screenId,
+//     minWidth: 200,
+//   },
+//   {
+//     label: 'Tính năng',
+//     field: FIX_BUG_DO_IMPROVEMENT_COLUMN_FIELD.featureId,
+//     minWidth: 200,
+//   },
+//   {
+//     label: 'Phân loại',
+//     field: FIX_BUG_DO_IMPROVEMENT_COLUMN_FIELD.tabName,
+//     minWidth: 200,
+//   },
+//   {
+//     label: 'Mã bug & improvement',
+//     field: FIX_BUG_DO_IMPROVEMENT_COLUMN_FIELD.code,
+//     minWidth: 120,
+//   },
+//   {
+//     label: 'Tên bug & improvement',
+//     field: FIX_BUG_DO_IMPROVEMENT_COLUMN_FIELD.name,
+//     minWidth: 120,
+//   },
+//   {
+//     label: 'Thời gian bắt đầu',
+//     field: FIX_BUG_DO_IMPROVEMENT_COLUMN_FIELD.startTime,
+//     minWidth: 200,
+//   },
+//   {
+//     label: 'Thời gian hoàn thành',
+//     field: FIX_BUG_DO_IMPROVEMENT_COLUMN_FIELD.endTime,
+//     minWidth: 200,
+//   },
+//   {
+//     label: 'Thời lượng',
+//     field: FIX_BUG_DO_IMPROVEMENT_COLUMN_FIELD.duration,
+//     minWidth: 120,
+//   },
+// ];
+
+export const fixBugDoImprovementHeaderColumnConfigs: IColumnHeaderConfigs[] =
+  getHeaderColumnConfigsFactory([
+    {
+      label: 'Mã bug & improvement',
+      field: FIX_BUG_DO_IMPROVEMENT_COLUMN_FIELD.code,
+      minWidth: 120,
+    },
+    {
+      label: 'Tên bug & improvement',
+      field: FIX_BUG_DO_IMPROVEMENT_COLUMN_FIELD.name,
+      minWidth: 120,
+    },
+    {
+      label: 'Hiện trạng',
+      field: FIX_BUG_DO_IMPROVEMENT_COLUMN_FIELD.name,
+      minWidth: 120,
+    },
+  ]);
